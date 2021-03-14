c04a694a05bbb2d3d91e9b122ec5a038b1eb8ce7
zaxwebs <zifidigital@gmail.com>
2021-03-14 14:42:01 +0530

    split messages

---
+++ slack/js/messages.js
@@ -0,0 +1,49 @@
+const initialMessages = [
+	{
+		conversationId: "c-0",
+		userId: 0,
+		text: "Holla",
+	},
+	{
+		conversationId: "c-0",
+		userId: 1,
+		text:
+			"Welcome to Slackish! A slack-ish clone built with Tailwind and Alpine.",
+	},
+	{
+		conversationId: "c-0",
+		userId: 2,
+		text:
+			"Head over to the features channel to see a list of features covered or added.",
+	},
+	{
+		conversationId: "c-0",
+		userId: 3,
+		text: "Thanks for checking this out.",
+	},
+	{
+		conversationId: "c-1",
+		userId: 0,
+		text: "Display user status indicator for DMs.",
+	},
+	{
+		conversationId: "c-1",
+		userId: 0,
+		text: "Indicate active conversation in sidebar",
+	},
+	{
+		conversationId: "u-0",
+		userId: 0,
+		text: "This is my personal message space.",
+	},
+	{
+		conversationId: "u-1",
+		userId: 1,
+		text: "Let's serve this on Netlify.",
+	},
+	{
+		conversationId: "u-1",
+		userId: 0,
+		text: "Sure!",
+	},
+]
